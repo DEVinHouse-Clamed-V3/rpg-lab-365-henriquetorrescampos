c6f7054577d6ba79d55243560b95d04136ebce7b
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-11-22 19:53:13 -0300

    publishing branchs

---
+++ src/class/Arma.ts
@@ -0,0 +1,23 @@
+class Arma {
+  private readonly name: string = "";
+  private damage: number = 0;
+  private readonly description: string = "";
+
+  getName() {
+    return this.name;
+  }
+  getDamage() {
+    return this.damage;
+  }
+  getDescription() {
+    return this.description;
+  }
+
+  setDamage(damage: number) {
+    if (damage < 0) {
+      console.log("Dano não pode ser negativo");
+    } else {
+      this.damage = damage;
+    }
+  }
+}
